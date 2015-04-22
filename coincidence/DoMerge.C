62bd125138586bf73deb66718e8f154a7681c623
fnoferin <fnoferin@cern.ch>
2015-04-22 13:37:57 +0200

    file to merge

---
+++ coincidence/DoMerge.C
@@ -0,0 +1,18 @@
+#include <TGrid.h>
+
+void DoMerge(char* nomelista="lista",char* output="outputMerge.root"){
+  FILE *f = fopen(nomelista,"r");
+
+  TFileMerger m(kFALSE);
+  m.OutputFile(output);
+
+  Int_t i=0;
+  char nome[100];
+  while (fscanf(f,"%s",nome)==1) {
+    m.AddFile(nome);
+    i++;
+  }
+  if (i)
+    m.Merge();
+}
+
