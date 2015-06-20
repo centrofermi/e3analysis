9ab6e721999cb650bf27a4e331488e229b03ffc8
Luca Baldini <luca.baldini@pi.infn.it>
2015-06-19 23:32:58 -0700

    Minor refactoring.

--- upgoingmu/beta_sanity_check.py
+++ upgoingmu/beta_sanity_check.py
@@ -1,11 +1,8 @@
-
 import os
 import ROOT
 
-DATA_FOLDER = '/data/work/EEE/data/upgoingmu'
-STATIONS = ['FRAS-02', 'SAVO-01', 'SAVO-02', 'SAVO-03']
-BETA_EXPR = 'TrackLength/TimeOfFlight/29979245800.e-9'
-CUT_EXPR = 'ChiSquare <= 10 && StatusCode == 0 && TimeOfFlight > 0'
+from __common__ import *
+
 OUTPUT_FILE_PATH = 'beta_sanity_check.root'
 
 print 'Opening output file %s...' % OUTPUT_FILE_PATH
