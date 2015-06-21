608ba1f709a54d3094ea0eafda80408e834d951e
Luca Baldini <luca.baldini@pi.infn.it>
2015-06-21 08:44:02 -0700

    Minor.

--- upgoingmu/__common__.py
+++ upgoingmu/__common__.py
@@ -2,4 +2,5 @@
 DATA_FOLDER = '/data/work/EEE/data/upgoingmu'
 STATIONS = ['FRAS-02', 'SAVO-01', 'SAVO-02', 'SAVO-03']
 BETA_EXPR = 'TrackLength/TimeOfFlight/29979245800.e-9'
-CUT_EXPR = 'ChiSquare <= 10 && StatusCode == 0 && TimeOfFlight > 0'
+BASE_CUT_EXPR = 'ChiSquare <= 10 && StatusCode == 0'
+CUT_EXPR = '%s && TimeOfFlight > 0' % BASE_CUT_EXPR 
