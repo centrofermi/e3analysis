e7bf9ba4cc6e9008736d4a123d9ecb1efa684a4f
Luca Baldini <luca.baldini@pi.infn.it>
2015-06-21 16:44:58 -0700

    Minor.

--- upgoingmu/mu_lifetime.py
+++ upgoingmu/mu_lifetime.py
@@ -5,7 +5,8 @@ import ROOT
 from __common__ import *
 
 OUTPUT_FILE_PATH = 'mu_lifetime.root'
-CUT_EXPR = '%s && TimeOfFlight < 0' % BASE_CUT_EXPR
+LOOSE_CUT_EXPR = '%s && TimeOfFlight < 0' % BASE_CUT_EXPR
+TIGHT_CUT_EXPR = '%s && NumClustersTop == 1 && NumClustersMid == 1 && NumClustersBot == 1' % LOOSE_CUT_EXPR
 
 print 'Opening output file %s...' % OUTPUT_FILE_PATH
 outputFile = ROOT.TFile(OUTPUT_FILE_PATH, 'RECREATE')
@@ -19,8 +20,17 @@ for station in STATIONS:
     hmutau = ROOT.TH1F(hname, station, 100, binning)
     hmutau.SetXTitle('TDP [2]')
     hmutau.SetYTitle('Entries/bin')
-    t.Project(hname, 'DeltaTime', CUT_EXPR)
+    t.Project(hname, 'DeltaTime', LOOSE_CUT_EXPR)
     print 'Writing %s...' % hname
     hmutau.Write()
+    print 'Done.'
+    hname = 'hmutau_onehit_%s' % station
+    binning = numpy.logspace(-7, 0, 101)
+    hmutauonehit = ROOT.TH1F(hname, station, 100, binning)
+    hmutauonehit.SetXTitle('TDP [2]')
+    hmutauonehit.SetYTitle('Entries/bin')
+    t.Project(hname, 'DeltaTime', TIGHT_CUT_EXPR)
+    print 'Writing %s...' % hname
+    hmutauonehit.Write()
     print 'Done.'
     f.Close()
